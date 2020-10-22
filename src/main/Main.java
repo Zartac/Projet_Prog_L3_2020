dbb1293b27ad3f1387936bc0b6282956d1a0547a
Jack <56638954+JackThay@users.noreply.github.com>
2020-10-22 22:25:40 +0200

    demo Github sauvage

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
 	public static void main(String[]args)
 	{
+		//ordre alphabet
 		char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 	}
 
