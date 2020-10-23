1f6c2ecc6500f2d0bd9c4f3449a8eda2ee7da3a0
Zartac <laforgejohan7@gmail.com>
2020-10-23 16:47:37 +0200

    poursuite test 4

--- src/testgithub/TestSuppression.java
+++
@@ -1,7 +0,0 @@
-package testgithub;
-
-public class TestSuppression {
-		private static void nerienfaire() {
-			//ne fait rien
-		}
-}
--- src/town/TestGit.java
+++
@@ -1,5 +0,0 @@
-package town;
-
-public class TestGit {
-		//Test upload and download fichier Github
-}
