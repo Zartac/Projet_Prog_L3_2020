6aaf049a6d80a4799d1f865c9f520ce86f67fb1b
Zartac <laforgejohan7@gmail.com>
2020-10-23 16:21:49 +0200

    Poursuite test Git

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String[]args)
 	{
 		//ordre alphabet
-		char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+		char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K'
+				,'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+		//Test Github
 	}
 
 }
