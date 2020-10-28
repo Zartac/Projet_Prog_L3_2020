a7111fd3b47de8def54b0a5200250675968b9bcc
JackThay <jacky.thay@yahoo.com>
2020-10-28 19:08:04 +0100

    Amélioration des test, de l'affichage du sujet, de la Javadoc, création de la question 1, protection contre 0 villes et moins.

--- src/menu/Display.java
+++ src/menu/Display.java
@@ -1,11 +1,21 @@
 package menu;
 
+/**
+ * Classe pour l'affichage du sujet et des invites de commandes utilisateurs.
+ * 
+ * @author Amanda Messahel, Johan Laforge, Jack Thay
+ * 
+ *
+ */
 public class Display 
 {
+	/**
+	 * Affichage de la problématique menant à ce projet.
+	 */
 	public static void displayLaunch() // Oui, je pourrais améliorer ça... -- Jack
 	{
 		System.out.println("|=============================================================|");
-		System.out.println("|=========Projet Programmation: Construction d'écoles=========|");
+		System.out.println("|======== Projet Programmation: Construction d'écoles ========|");
 		System.out.println("|=============================================================|");
 		System.out.println("|                      - Amanda Messahel                      |");
 		System.out.println("|                       - Johan Laforge                       |");
@@ -13,16 +23,30 @@ public class Display
 		System.out.println("|=============================================================|");
 		System.out.println("|Problématique:                                               |");
 		System.out.println("|Dans le cadre d’une politique ambitieuse concernant          |");
-		System.out.println("|l’éducation, un élu souhaite construire de de nouvelles      |");
-		System.out.println("|écoles, modernes, dans la communauté d’agglomération dont il |");
-		System.out.println("|est responsable. Durant la campagne électorale, il a promis  |");
-		System.out.println("|deux choses :                                                |");
+		System.out.println("|l’éducation, un élu souhaite construire de nouvelles écoles, |");
+		System.out.println("|modernes, dans la communauté d’agglomération dont il est     |");
+		System.out.println("|responsable.                                                 |");
+		System.out.println("|Durant la campagne électorale, il a promis deux choses:      |");
 		System.out.println("| -Chaque ville doit posséder son école, ou être directement  |");
 		System.out.println("|  reliée à une ville qui possède une école.                  |");
 		System.out.println("| -Le coût du projet doit être le plus bas possible, ce qui   |");
 		System.out.println("|  signifie que le nombre d’écoles à construire               |");
 		System.out.println("|  doit être le plus petit possible.                          |");
 		System.out.println("|=============================================================|");		
+	}
+	
+	/**
+	 * Affichage de la création de liens entre villes.
+	 */
+	public static void displayInterface1() // Les options suivantes sont celle INDIQUE par le sujet! -- Jack
+	{
+		System.out.println("");
+		System.out.println("|=============================================================|");
+		System.out.println("|============== Création de route entre villes ===============|");
+		System.out.println("|=============================================================|");
+		System.out.println("|1. Ajouter une route.                                        |");
+		System.out.println("|2. Fin.                                                      |");
+		System.out.println("|=============================================================|");
 	}
 
 }
--- src/town/TownTest.java
+++ src/town/TownTest.java
@@ -14,10 +14,10 @@ class TownTest {
 		for (int i = 0; i < tab.length; ++i) 
 		{
 			tab[i] = new Town(alphabet[i]); // On crée 10 ville
-			assertEquals(tab[i].isSchool(), false); // On vérifie que chaque ville n'a pas d'école
-			assertEquals(tab[0].getName(), 'A'); // On vérifie que la 1ere ville est nommé 'A'
-			assertEquals(tab[0].getSerial(), 1); // On vérifie que la 1ere ville a un serial "1"
-			assertEquals(tab[i].getName(), alphabet[i]); // On vérifie que chaque ville a un nom qui correspond à l'alphabet
+			assertEquals(false, tab[i].isSchool()); // On vérifie que chaque ville n'a pas d'école
+			assertEquals('A', tab[0].getName()); // On vérifie que la 1ere ville est nommé 'A'
+			assertEquals(1, tab[0].getSerial()); // On vérifie que la 1ere ville a un serial "1"
+			assertEquals(alphabet[i], tab[i].getName()); // On vérifie que chaque ville a un nom qui correspond à l'alphabet
 			for (int j = 0; j < i; ++j)
 				assertNotEquals(tab[i].getSerial(), tab[j].getSerial()); // On vérifie que chaque ville a un serial différent
 		}
