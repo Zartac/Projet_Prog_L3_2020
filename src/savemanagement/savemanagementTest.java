5ecfb38694e5e8aa5f8da785c0166d27ab6c9ed5
lafor <lafor@DESKTOP-LKB37GG.home>
2020-12-11 00:40:49 +0100

    Ajout du test (useless) - Johan

---
+++ src/savemanagement/savemanagementTest.java
@@ -0,0 +1,15 @@
+package savemanagement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class savemanagementTest {
+
+	@Test
+	void test() {
+		String a="";
+		fail("Not yet implemented");
+	}
+
+}
